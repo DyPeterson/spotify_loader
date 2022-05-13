ab210963b74308c58ed5775b11ccd2add2bba07f
DyPeterson <dylan.peterson17@gmail.com>
2022-05-13 14:50:04 -0700

    add base function to merge tables, still having error 1054

--- main.py
+++ main.py
@@ -33,12 +33,13 @@ class DataLoader():
             index_name (str): the index column name
             colum_names (list): list of columns to concatenate into an index column
         """
+        #
         df = self.df
-        # add multiple column names together with a '-' to create unique columns
-        df[index_name] = df[column_names].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
-        df[index_name] = df[column_names]
-        for column in column_names:
-            new_column_name = '-'.join(column)
+        # add multiple column names together with a "-" to create unique columns
+        df[index_name] = df[column_names].apply(lambda row: "-".join(row.values.astype(str)), axis=1)
+        
+        # for column in column_names:
+        #     new_column_name = "-".join(column)
         # use unique column to set as index
         df.set_index(index_name, inplace=True)
         self.df = df
@@ -67,8 +68,13 @@ class DataLoader():
         df = self.df
         # write to sql taking db table name as the name, db_engine as the engine if this already exists append it to existing table
         # write 2000 rows at a time.
-        df.to_sql(db_table_name, db_engine, if_exists='append', chunksize=2000)
+        df.to_sql(db_table_name, con=db_engine, if_exists="append", chunksize=2000)
+        self.df = self
 
+    # def merge_tables(self, dataframe, left_on, right_on, join_cols, how='left'):
+    #     """
+    #     Merge DataFrames on specific columns with multiple csvs, limited to an output of 20
+    #     """
 
 
 def db_engine(db_host:str, db_user:str, db_pass:str, db_name:str="spotify") -> sa.engine.Engine:
@@ -83,7 +89,7 @@ def db_engine(db_host:str, db_user:str, db_pass:str, db_name:str="spotify") -> s
     Returns:
         sa.engine.Engine: sqlalchemy engine
     """
-    engine = sa.create_engine(f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}', future=True) 
+    engine = sa.create_engine(f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}", future=True) 
     return engine
 
 
@@ -102,34 +108,34 @@ def db_create_tables(db_engine, drop_first:bool = False) -> None:
     # your code to define tables go in here
     #   - Be careful, some of the columns like album.available_markets are very long. Make sure you give enough DB length for these. ie: 10240 (10kb)
     # 
-    albums_table = Table('albums',
+    albums_table = Table("albums",
         meta,
-        Column('album_type', String(256), primary_key=True),
-        Column('artist_id', String(256)),
-        Column('available_markets', String(1024)),
-        Column('external_urls', String(512)),
-        Column('href', String(256)),
-        Column('id', String(256)),
-        Column('images', String(1024)),
-        Column('name', String(1024)),
-        Column('release_date', Date),
-        Column('release_date_precision', String(128)),
-        Column('total_tracks', Numeric),
-        Column('track_id', String(256)),
-        Column('track_name_prev', String(256)),
-        Column('uri', String(256)),
-        Column('type', String(256))
+        Column("artist_id", String(256), primary_key=True),
+        Column("album_type", String(256)),
+        Column("available_markets", String(1024)),
+        Column("external_urls", String(512)),
+        Column("href", String(256)),
+        Column("id", String(256)),
+        Column("images", String(1024)),
+        Column("name", String(1024)),
+        Column("release_date", Date),
+        Column("release_date_precision", String(128)),
+        Column("total_tracks", Numeric),
+        Column("track_id", String(256)),
+        Column("track_name_prev", String(256)),
+        Column("uri", String(256)),
+        Column("type", String(256))
     )
-    artists_table = Table('artists',
+    artists_table = Table("artists",
         meta,
-        Column('artist_popularity', Numeric),
-        Column('followers', Numeric),
-        Column('genres', String(1024)),
-        Column('id', String(256)),
-        Column('name', String(256)),
-        Column('track_id', String(128)),
-        Column('track_name_prev', String(128)),
-        Column('type', String(128)),
+        Column("id", String(256), primary_key=True),
+        Column("artist_popularity", Numeric),
+        Column("followers", Numeric),
+        Column("genres", String(1024)),
+        Column("name", String(256)),
+        Column("track_id", String(128)),
+        Column("track_name_prev", String(128)),
+        Column("type", String(128)),
         )
     # your code to drop and create tables go here
     if drop_first:
@@ -152,26 +158,26 @@ def main():
     - creates database metadata tables/columns
     - loads both artists and albums into database
     """
-    artist_filepath = './data/spotify_artists.csv'
-    album_filepath = './data/spotify_albums.csv'
-    # instance data loader for both csv's
+    artist_filepath = "./data/spotify_artists.csv"
+    album_filepath = "./data/spotify_albums.csv"
+    # instance data loader for both csv"s
     artist_data = DataLoader(artist_filepath)
     album_data = DataLoader(album_filepath)
     # print head of both
     artist_data.head()
     album_data.head()
     # set index of both data sets
-    artist_data.add_index('artist_index',['id'])
-    album_data.add_index('albums_index',['artist_id','id','release_date'])
+    artist_data.add_index("artist_index", ["id"])
+    album_data.add_index("albums_index", ["artist_id","id","release_date"])
     # sort artist by name
-    artist_data.sort('name')
+    artist_data.sort("name")
     # create db engine
-    engine = db_engine(db_host='127.0.0.1', db_user='root', db_pass='mysql', db_name='spotify')
+    engine = db_engine(db_host="127.0.0.1", db_user="root", db_pass="mysql", db_name="spotify")
     # create db metadata table/columns
     db_create_tables(engine)
     # load both in db
-    artist_data.load_to_db(engine,'artists')
-    album_data.load_to_db(engine,'albums')
+    artist_data.load_to_db(engine, "artists")
+    album_data.load_to_db(engine, "albums")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
